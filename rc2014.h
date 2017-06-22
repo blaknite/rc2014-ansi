43e2406476c4b6d52b1500592ce6e74421ba146f
Grant Colegate <blaknite@thelanbox.com.au>
2017-06-22 21:52:44 +0930

    Make proper use of lib and fastcall

--- rc2014.h
+++ rc2014.h
@@ -2,19 +2,19 @@
 #define _RC2014_H_
 
 // UART Routines
-extern unsigned char __LIB__ __FASTCALL__ rc2014_uart_rx_ready();
-extern unsigned char __LIB__ __FASTCALL__ rc2014_uart_rx();
+extern unsigned char __LIB__ __FASTCALL__ rc2014_uart_rx_ready(void);
+extern unsigned char __LIB__ __FASTCALL__ rc2014_uart_rx(void);
 extern void __LIB__ __FASTCALL__ rc2014_uart_tx(unsigned char c);
 
 // Print Routines
-void rc2014_print(unsigned char *str);
+void __LIB__ __FASTCALL__ rc2014_print(unsigned char *str);
 
 // ANSI Terminal Routines
-void rc2014_ansi_cmd(unsigned char *cmd);
-void rc2014_ansi_cls(void);
-void rc2014_ansi_hide_cursor(void);
-void rc2014_ansi_show_cursor(void);
-void rc2014_ansi_move_cursor(unsigned int row, unsigned int col);
-void rc2014_ansi_color(unsigned int col);
+void __LIB__ __FASTCALL__ rc2014_ansi_cmd(unsigned char *cmd);
+void __LIB__ __FASTCALL__ rc2014_ansi_cls(void);
+void __LIB__ __FASTCALL__ rc2014_ansi_hide_cursor(void);
+void __LIB__ __FASTCALL__ rc2014_ansi_show_cursor(void);
+void __LIB__ rc2014_ansi_move_cursor(unsigned int row, unsigned int col);
+void __LIB__ __FASTCALL__ rc2014_ansi_color(unsigned int col);
 
 #endif
